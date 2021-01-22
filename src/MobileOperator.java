dd9fccf2169a2bcfc7e038ccc2b472b7bb2ee6e2
OksanaTarun <oxana.tarun@gmail.com>
2021-01-22 13:25:16 +0300

    Mobile Operator

---
+++ src/MobileOperator.java
@@ -0,0 +1,13 @@
+public class MobileOperator {
+    public static void main(String[] args) {
+        int initialbalance = 100;
+        int refill = 1100;
+        int bonus;
+        if (refill >= 1000) {
+            bonus = refill / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+    }
+}
